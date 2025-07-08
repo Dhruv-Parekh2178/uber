374bb3539ef4f82f88b5cdabdb198eab5201b586
VaibhaviShopify-rundtc <vaibhavi.parekh@rundtc.com>
2025-07-09 00:28:56 +0530

    vercel error done

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,12 @@ const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
